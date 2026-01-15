031739d6a94f530d02cc9dea5ef9047008ca537d
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-01-14 21:55:01 -0300

    feat: adds API search bar recommendations by script.js and changes on style

---
+++ script.js
@@ -0,0 +1,69 @@
+//Pegando o input de texto HTML e a <ul>
+var filmInput = document.querySelector("#film_name")
+var filmList = document.querySelector("#film_list")
+filmInput.addEventListener("input", searchFilm)
+
+function searchFilm() {
+  var filmId = filmInput.value.trim().toLowerCase()
+  if (filmId.length < 2) {
+    filmList.innerHTML = ""
+    return
+  }
+  filmList.innerHTML = ""
+
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?include_adult=false&language=pt-BR&query=${filmId}&page=1&api_key=process.env.API_KEY
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let popularityRank = []
+      for (let i = 0; i < data.results.length; i++) {
+        popularityRank.push(data.results[i].popularity)
+      }
+      //ordenando o rank de relevância dos filmes
+      popularityRank.sort((a, b) => a - b)
+      popularityRank.reverse()
+      //inserindo os 8 mais relevantes como recomendação
+      //usando o rank de relevância para selecionar as recomendações da API que quero aparecendo nas <li>
+      for (let i = 0; i < popularityRank.length; i++) {
+        for (let j = 0; j < data.results.length; j++) {
+          if (
+            popularityRank[i] == data.results[j].popularity &&
+            data.results[j].release_date.length > 0 &&
+            filmList.children.length < 7 &&
+            data.results[j].title.trim().toLowerCase().includes(filmId)
+          ) {
+            const listItem = document.createElement("li")
+
+            const movie_title = document.createElement("span")
+            movie_title.classList.add("movie_title")
+            movie_title.textContent = data.results[j].title
+
+            const release_date = document.createElement("span")
+            release_date.classList.add("movie_release_date")
+            release_date.textContent = `(${data.results[j].release_date.slice(
+              0,
+              4
+            )})`
+
+            listItem.append(movie_title, release_date)
+            listItem.addEventListener("click", () => {
+              recommendFilms(data.results[j])
+            })
+            filmList.appendChild(listItem)
+          }
+        }
+      }
+    })
+    .catch((error) => console.error(error))
+}
+
+function recommendFilms(data) {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${data.id}/recommendations?page=1&language=en-US&api_key=process.env.API_KEY
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+    })
+}
